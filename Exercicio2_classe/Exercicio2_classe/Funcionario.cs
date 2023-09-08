5eac1eabbcf0ec46e2d83d2048d954705a2ed442
Henrique Bisetto <132086713+hbisetto@users.noreply.github.com>
2023-09-08 15:45:26 -0300

    Add files via upload

---
+++ Exercicio2_classe/Exercicio2_classe/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TAD
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ Exercicio2_classe/Exercicio2_classe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Channels;
+using TAD;
+
+namespace Exercicio2_classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1, f2;
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+            
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.WriteLine("Nome: ");
+            f2.nome = Console.ReadLine();
+            Console.WriteLine("Salário: ");
+            f2.salario = double.Parse(Console.ReadLine());
+
+            double mediaSalarial = (f1.salario + f2.salario) / 2;
+            Console.WriteLine("Salário médio = {0:F2}", mediaSalarial);
+
+        }
+    }
+}
